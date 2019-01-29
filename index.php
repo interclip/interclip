87163b18ad532db7e22bc0f5d38656cef7925a03
Filip Troníček <filip.tronicek@seznam.cz>
2019-01-29 20:45:09 +0100

    Changed HTML lang

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="cs" >
+<html lang="en" >
 <script
         src="https://code.jquery.com/jquery-3.3.1.min.js"
         integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
