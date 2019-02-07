7ef16d46ebb8090f213c1bf089ea0e4e13ab7377
Filip Troníček <filip.tronicek@seznam.cz>
2019-02-07 20:00:35 +0100

    Design fixes
    Everything was just..... centered

--- index.php
+++ index.php
@@ -1,23 +1,10 @@
 <!DOCTYPE html>
 <html lang="en" >
 <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
-    
-<script type="text/javascript">
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 
-  function readFiles()
-{
-    $.get('clip.txt', function(data) {
-   //  var dataarray = split(data.split("."));
-    // var datatype = dataarray[dataarray.length - 1];
-        document.getElementById("clipboard").innerHTML += data;
-    }, "text");
-}
-
-</script>
 <head>
   <meta charset="UTF-8">
   <title>Interclip</title>
@@ -29,7 +16,7 @@
     <link rel="shortcut icon" href="./img/favicon.ico">
 </head>
 
-<body>
+<body id="body">
     <div class="title">
       
     </div>
@@ -45,19 +32,18 @@
 
   
 </head>
+<center>
 <div id="hidden">
   <endora></endora>
 </div>
-<body>
-  <script type="text/javascript">
-
-    readFiles();
-  </script>
-  <p id="clipboard">
+<body onload="readFiles()">
+ 
+  <p class="text-center" id="clipboard">
 <span> Your clipboard:<span>
 <br />
 <div id="clip-content"></div>
 </p>
+
   <form action="" method="GET">
 <div class="container">
   <p class="lb">Link</p>
@@ -66,11 +52,12 @@
 
   
 </div>
-<center>
+
   <button id="sv" class="btn btn-primary" onclick="form.submit();">
   Save
 </button>
   </form>
+  </center>
   <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 
   
@@ -80,28 +67,52 @@
 
 <div class="hidden"> <endora> </endora> </div>
 <a href="https://github.com/filiptronicek/Interclip" target="_blank" class="github-corner" aria-label="View source on Github">		<svg width="80" height="80" viewBox="0 0 250 250" style="fill:#151513; color:#fff; z-index:99; position: absolute; top: 0; border: 0; right: 0;" aria-hidden="true"><path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path><path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path><path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path></svg>	    </a>	    <style>.github-corner:hover .octo-arm{animation:octocat-wave 560ms ease-in-out}@keyframes octocat-wave{0%,100%{transform:rotate(0)}20%,60%{transform:rotate(-25deg)}40%,80%{transform:rotate(10deg)}}@media (max-width:500px){.github-corner:hover .octo-arm{animation:none}.github-corner .octo-arm{animation:octocat-wave 560ms ease-in-out}}</style>
+<script type="text/javascript">
+//DOM
 
+var clip = document.getElementById("clipboard");
+//The real shit
+
+  function readFiles()
+{
+    $.get('clip.txt', function(data) {
+    
+    /*  var dataarray = data.split(".");
+      var ext = dataarray[dataarray.length - 1];
+      var actextn = ext.split("&");
+      var ext = actextn[0];
+
+      if(ext == "jpg" || ext == "png") {
+        console.log("An image ("+ext+")");
+       //document.getElementById("body").innerHTML += '  <img class="img-responsive" src="'+data+'" alt="Chania" width="460" height="345"> ';
+      }
+      */
+        document.getElementById("clipboard").innerHTML += data;
+    }, "text");
+}
+
+</script>
 <?php 
 if(isset($_GET['url'])) {
 /*
 if(filter_var($text, FILTER_VALIDATE_URL))
 {
   */
-
+/*
   $long_url = $_GET['url'];
   $bitly_login = 'filiptronicek';
   $bitly_apikey = 'R_e8b7b028a203498f96129cbcda9f15e6';
   $uniqid = uniqid();
   $bitly_response = json_decode(file_get_contents("http://api.bit.ly/v3/shorten?login={$bitly_login}&apiKey={$bitly_apikey}&longUrl={$long_url}&format=json"));
   $short_url = $bitly_response->data->url;
+  */
 
   $myfile = fopen("clip.txt", "w") or die("A fucking error has occured. Either fuck yourself or submit an issue on GitHub");
-  $txt = $short_url;
+  $txt = urldecode($_GET['url']);
   fwrite($myfile, $txt);
   fclose($myfile);
 
 
-die("fug");
 /*echo($sunset);
 echo ($sunrise);
 */
