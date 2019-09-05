f7ff33bf432377bbc4d9f43d191d6b5a7b1786a3
Filip Troníček <filip.tronicek@seznam.cz>
2019-09-05 21:05:10 +0200

    API to directly get a code of a link

---
+++ api.php
@@ -0,0 +1,29 @@
+<?php
+if(isset($_POST['url'])) {
+  include("db.php");
+
+  
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $DBName);
+  
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+  function gen_uid($l=10){
+    return substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyz"), 0, $l);
+    }
+    $usr = gen_uid(5);
+    $url = $_POST['url'];
+    $timestamp = date("Y-m-d H:i:s");
+
+  $sqlquery = "INSERT INTO userurl (id, usr, url, date) VALUES (NULL, '$usr', '$url', '$timestamp') ";
+
+if ($conn->query($sqlquery) === TRUE) {
+} else {
+    echo "Error: " . $sqlquery . "<br>" . $conn->error;
+}  
+$conn->close();
+}
+echo $usr;
+?>
