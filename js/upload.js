8719b95d2958022551f7f586acb296c7ee19c6a3
Filip Troníček <filip.tronicek@seznam.cz>
2019-09-07 19:57:24 +0200

    Minor code adjustments

--- js/upload.js
+++ js/upload.js
@@ -2,9 +2,7 @@ var feedback = function(res) {
     if (res.success === true) {
         var get_link = res.data.link.replace(/^http:\/\//i, 'https://');
         document.querySelector('.status').classList.add('bg-success');
-       /* document.querySelector('.status').innerHTML +=
-            'Image : ' + '<br><input class="image-url" value=\"' + get_link + '\"/>' + '<img class="img" alt="Imgur-Upload" src=\"' + get_link + '\"/>';
-            */
+
            $.post("api.php",
            {
              url: get_link,
@@ -22,6 +20,6 @@ var feedback = function(res) {
 };
 
 new Imgur({
-    clientid: '4409588f10776f7', //You can change this ClientID
+    clientid: '4409588f10776f7', 
     callback: feedback
 });
