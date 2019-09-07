bd644036158cf906756495f3851bf4a147f91016
Filip Troníček <filip.tronicek@seznam.cz>
2019-09-07 12:49:39 +0200

    Added ;

--- js/index.js
+++ js/index.js
@@ -13,8 +13,8 @@ searchBtn.addEventListener("click", expand);
 
 function isValidURL(string) {
 	var res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-	return (res !== null)
-  };
+	return (res !== null);
+  }
   
 
 function validateForm() {
