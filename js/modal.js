9df2571459c3d934655e999fa27d1cafe9f60381
Filip Troníček <filip.tronicek@seznam.cz>
2019-09-07 19:57:54 +0200

    Reload page when closed modal on image upload

--- js/modal.js
+++ js/modal.js
@@ -4,11 +4,13 @@ var modal = document.getElementById("myModal");
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  location.reload(false); 
 };
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    location.reload(false); 
   }
 };
