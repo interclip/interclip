f126278852f5a135654a69d2796ad8ee4e3af1cb
Filip Troníček <filip.tronicek@seznam.cz>
2019-09-07 13:11:36 +0200

    Format

--- js/modal.js
+++ js/modal.js
@@ -3,12 +3,12 @@ var modal = document.getElementById("myModal");
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
     modal.style.display = "none";
-  };
-  
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  };
+  }
+};
