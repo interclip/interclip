2391659c3ae205c99a82b2cdb8cf5beef484d80b
Filip Troníček <filip.tronicek@seznam.cz>
2019-09-07 10:20:33 +0200

    Added details to the repo

---
+++ js/about.js
@@ -0,0 +1,8 @@
+$.get('https://api.github.com/repos/filiptronicek/Interclip/releases', {}, function(data) {
+    $("#version").append("<a href='"+data[0].html_url+"'>"+data[0].tag_name+"</a>");
+
+});
+$.get('https://api.github.com/repos/filiptronicek/Interclip/commits', {}, function(data) {
+
+$("#commit").append("<a href='"+data[0].html_url+"'>"+data[0].commit.message+"</a> by <a href='"+data[0].author.html_url +"'>" + data[0].commit.author.name + "</a>");
+});
