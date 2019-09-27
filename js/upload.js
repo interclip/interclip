a31039d488d78c5c9f22e6cb459a643bb9bca48d
Filip Troníček <filip.tronicek@seznam.cz>
2019-09-27 17:15:52 +0200

    Format

--- js/upload.js
+++ js/upload.js
@@ -1,25 +1,26 @@
 var feedback = function(res) {
-    if (res.success === true) {
-        var get_link = res.data.link.replace(/^http:\/\//i, 'https://');
-        document.querySelector('.status').classList.add('bg-success');
+  if (res.success === true) {
+    var get_link = res.data.link.replace(/^http:\/\//i, "https://");
+    document.querySelector(".status").classList.add("bg-success");
 
-           $.post("api.php",
-           {
-             url: get_link,
-           },
-           function(data,status){
-            console.log("Data: " + data + "\nStatus: " + status);
-            if(status == "success") {
-              $("img").attr("src", get_link);
-              $("#code").text(data);
-              $("#myModal").show();
-            }
- 
-           });
-    }
+    $.post(
+      "api.php",
+      {
+        url: get_link
+      },
+      function(data, status) {
+        console.log("Data: " + data + "\nStatus: " + status);
+        if (status == "success") {
+          $("img").attr("src", get_link);
+          $("#code").text(data);
+          $("#myModal").show();
+        }
+      }
+    );
+  }
 };
 
 new Imgur({
-    clientid: '4409588f10776f7', 
-    callback: feedback
+  clientid: "4409588f10776f7",
+  callback: feedback
 });
