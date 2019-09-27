e63beaa09676869dd48999558474efbe09596114
Filip Troníček <filip.tronicek@seznam.cz>
2019-09-27 18:28:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -16,11 +16,11 @@
   <li><a href="./recieve/">Recieve</a></li>
   <li style="float:right"><a href="about">About</a></li>
 </ul>
-
 <form name="urlform" id="content" onsubmit="return validateForm()" action="new" method="POST">
 
   <input type="text" name="input" class="input" id="search-input">
   <button type="reset" class="search" id="search-btn"></button>
+
 </form>
 <script src="js/index.js"> </script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
