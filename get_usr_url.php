13de99847a2a0ced9019aa45d3b5f2bab0f0e4a6
Filip Troníček <filip.tronicek@seznam.cz>
2019-09-29 20:59:27 +0200

    :bug: js location

--- get_usr_url.php
+++ get_usr_url.php
@@ -64,4 +64,4 @@ if (!empty($_POST['user'])) {
     </div>
   </div>
   <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-  <script src="../js/get_link.js"></script>
+  <script src="./js/get_link.js"></script>
