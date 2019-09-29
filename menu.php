a9f4cea11061b6a74f911d4f20670fcc03da4384
Filip Troníček <filip.tronicek@seznam.cz>
2019-09-29 19:45:39 +0200

    Added menu

---
+++ menu.php
@@ -0,0 +1,9 @@
+<ul>
+    <li><a href="./">Send</a></li>
+    <li><a class="active" href="#">Send image</a></li>
+    <li><a href="./file">Send file</a></li>
+
+    <li><a href="./recieve/">Recieve</a></li>
+
+    <li style="float:right"><a href="./about">About</a></li>
+</ul>
