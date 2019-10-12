6a333f1b2bc7174081015af0d2512d8b2c9c2e8f
Filip Troníček <filip.tronicek@seznam.cz>
2019-10-12 19:28:28 +0200

    Show analytics only when in production

---
+++ includes/analytics.php
@@ -0,0 +1,18 @@
+<script type="text/javascript">
+var _paq = window._paq || [];
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+    var u = "//nfogix.com/";
+    _paq.push(['setTrackerUrl', u + 'api/track']);
+    _paq.push(['setSiteId', '2accc6dc-f7cc-482c-8403-3687eac862cb']);
+    var d = document,
+        g = d.createElement('script'),
+        s = d.getElementsByTagName('script')[0];
+    g.type = 'text/javascript';
+    g.async = true;
+    g.defer = true;
+    g.src = u + 'js/nfogix.min.js';
+    s.parentNode.insertBefore(g, s);
+})();
+</script>
--- includes/menu.php
+++ includes/menu.php
@@ -1,21 +1,3 @@
-<script type="text/javascript">
-var _paq = window._paq || [];
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-(function() {
-    var u = "//nfogix.com/";
-    _paq.push(['setTrackerUrl', u + 'api/track']);
-    _paq.push(['setSiteId', '2accc6dc-f7cc-482c-8403-3687eac862cb']);
-    var d = document,
-        g = d.createElement('script'),
-        s = d.getElementsByTagName('script')[0];
-    g.type = 'text/javascript';
-    g.async = true;
-    g.defer = true;
-    g.src = u + 'js/nfogix.min.js';
-    s.parentNode.insertBefore(g, s);
-})();
-</script>
 <?php
 if(!isset($prodvar)) {
     include('prod.php');
@@ -34,6 +16,7 @@ if (!isProd()) {
     $scriptNameArray = explode("\\", $scriptPath);
 } else {
     $scriptNameArray = explode("/", $scriptPath);
+    include_once('analytics.php');
 }
 $currFile = end($scriptNameArray);
 
