7bc164cc4eef3c7351fb138add1281688cad4a62
Filip Troníček <filip.tronicek@seznam.cz>
2019-10-16 19:23:28 +0200

    Format

--- about.php
+++ about.php
@@ -1,7 +1,7 @@
  <!-- Compiled and minified CSS -->
  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
  <link rel="stylesheet" href="./css/about.css">
-
+<link rel="stylesheet" href="css/dark.css">
  <html>
 
  <head>
@@ -12,8 +12,8 @@
  </head>
  <div class="nav">
      <?php
-include("includes/menu.php");
-?>
+        include("includes/menu.php");
+        ?>
  </div>
  <br>
 
@@ -31,8 +31,7 @@ include("includes/menu.php");
 
          </div>
 
-         <p>From <a href="http://filiptronicek.me/interclip-3/"> the article</a> on <a
-                 href="http://filiptronicek.me">filiptronicek.me </a></p>
+         <p>From <a href="http://filiptronicek.me/interclip-3/"> the article</a> on <a href="http://filiptronicek.me">filiptronicek.me </a></p>
          <p>
              <p>Soooo...
                  I was really excited for this release, mainly because a lot of people I talked to said it is a great
@@ -52,18 +51,12 @@ include("includes/menu.php");
              <p>After that I figured out that the code that is used to access the urls from the database should be 5
                  digit base 36 strings, so that makes about 60 466 176 combinations - and I want to make a cron script,
                  that deletes all the codes after a week in the next major release.</p>
-             <p><img src="https://user-images.githubusercontent.com/29888641/62826686-faa86b80-bbbf-11e9-81db-c7e321eaf2e4.png"
-                     alt="image">
-                 <img src="https://user-images.githubusercontent.com/29888641/62826693-127fef80-bbc0-11e9-95fe-300241e31397.png"
-                     alt="image">
-                 <img src="https://user-images.githubusercontent.com/29888641/62826696-24619280-bbc0-11e9-81d1-e99c1c37e0d1.png"
-                     alt="image">
-                 <img src="https://user-images.githubusercontent.com/29888641/62826699-36433580-bbc0-11e9-925b-00c7f0e4e8f4.png"
-                     alt="image">
-                 <img src="https://user-images.githubusercontent.com/29888641/62826703-4e1ab980-bbc0-11e9-9bc1-eb102f8059fa.png"
-                     alt="image">
-                 <img src="https://user-images.githubusercontent.com/29888641/62826710-6094f300-bbc0-11e9-8511-59d12efb8618.png"
-                     alt="image"></p>
+             <p><img src="https://user-images.githubusercontent.com/29888641/62826686-faa86b80-bbbf-11e9-81db-c7e321eaf2e4.png" alt="image">
+                 <img src="https://user-images.githubusercontent.com/29888641/62826693-127fef80-bbc0-11e9-95fe-300241e31397.png" alt="image">
+                 <img src="https://user-images.githubusercontent.com/29888641/62826696-24619280-bbc0-11e9-81d1-e99c1c37e0d1.png" alt="image">
+                 <img src="https://user-images.githubusercontent.com/29888641/62826699-36433580-bbc0-11e9-925b-00c7f0e4e8f4.png" alt="image">
+                 <img src="https://user-images.githubusercontent.com/29888641/62826703-4e1ab980-bbc0-11e9-9bc1-eb102f8059fa.png" alt="image">
+                 <img src="https://user-images.githubusercontent.com/29888641/62826710-6094f300-bbc0-11e9-8511-59d12efb8618.png" alt="image"></p>
          </p>
      </div>
      <!-- Compiled and minified JavaScript -->
