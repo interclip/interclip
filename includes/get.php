b45a479658a8a458eb7c87eea4f0b330ece0fc46
Filip Troníček <filip.tronicek@seznam.cz>
2019-10-16 19:13:39 +0200

    Format

--- includes/get.php
+++ includes/get.php
@@ -32,10 +32,10 @@ include_once "components/get.php";
 
         </div>
     </div>
-  </div>
-  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-  <script src="https://cdn.jsdelivr.net/gh/filiptronicek/Embed/embed.js"> </script>
-  <script src="../js/get.js"></script>
-  <script> 
+</div>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src="https://cdn.jsdelivr.net/gh/filiptronicek/Embed/embed.js"> </script>
+<script src="../js/get.js"></script>
+<script>
 Embed($("#urlLink").text());
-  </script>
+</script>
