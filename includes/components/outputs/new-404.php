d4d973527bc24ebecd3a524fdf778426bdf84bad
Filip Troníček <filip.tronicek@seznam.cz>
2019-10-26 13:06:05 +0200

    Update new-404.php

--- includes/components/outputs/new-404.php
+++ includes/components/outputs/new-404.php
@@ -1,1 +1,2 @@
-<h1 class="errheader"><span>4</span>&nbsp;<span>0</span>&nbsp;<span>0</span> </h1><br> <span id="errcode">bad request</span>
+<h1 class="errheader"><span>4</span>&nbsp;<span>0</span>&nbsp;<span>0</span> </h1><br> <span id="errcode">bad
+    request</span>
