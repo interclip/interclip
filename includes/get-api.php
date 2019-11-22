4aad3ec686790bcecfacb81a9a52336717041529
Filip Troníček <filip.tronicek@seznam.cz>
2019-11-22 21:53:36 +0100

    Update get-api.php

--- includes/get-api.php
+++ includes/get-api.php
@@ -1,12 +1,14 @@
-<?php 
+<?php
 if (!empty($_POST['user'])) {
-    $user_code = $_POST['user'];
-    include_once "components/get.php";
-  }
-if(isset($url)) {
-echo $url;
+  $user_code = $_POST['user'];
+} elseif (!empty($_GET['user'])) {
+  $user_code = $_GET['user'];
+}
+include_once "components/get.php";
+if (isset($url)) {
+  echo $url;
 } else {
-    http_response_code(400);
-    echo "Error: no code given";
-    die();
+  http_response_code(400);
+  echo "Error: no code given";
+  die();
 }
