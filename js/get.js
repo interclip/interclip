d79277aa4dc594253019ec3790b7a22c0e81ec20
Filip Troníček <filip.tronicek@seznam.cz>
2020-05-21 11:13:45 +0200

    Update get.js

--- js/get.js
+++ js/get.js
@@ -1,6 +1,6 @@
-var str = $("#urlLink").text();
-var https = str.indexOf("https");
-var http = str.indexOf("http");
+let str = $("#urlLink").text();
+const https = str.indexOf("https");
+const http = str.indexOf("http");
 
 if (https > -1) {
     str = str.split("https://").pop();
