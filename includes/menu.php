b6bfb72a27b6ee48511ba278aac021998893da7f
Filip Troníček <filip.tronicek@seznam.cz>
2020-09-07 22:30:25 +0200

    Update menu.php

--- includes/menu.php
+++ includes/menu.php
@@ -41,7 +41,7 @@ foreach ($pages as $page) {
     } else {
         if ($page[0] == "about") {
             echo '<li style="float:right"><a href="' . $urlPrefix . 'about">About</a></li></ul>';
-        } elseif ($page[0] == "desktop") { 
+        } elseif ($page[0] == "desktop") {
             echo '<li id="desktopMenuItem" style="display: none;"><a href="' . $urlPrefix . $page[0] . '">' . $page[1] . '</a></li> ';
         } else {
             echo '<li><a href="' . $urlPrefix . $page[0] . '">' . $page[1] . '</a></li> ';
@@ -59,11 +59,4 @@ echo '<div id="endora" style="display: none"><endora></div>';
 <script src='https://cdn.jsdelivr.net/gh/jquery/jquery/dist/jquery.min.js'></script>
 <!--<script src="js/renderer.js"></script> -->
 <script type="module" src="https://cdn.pika.dev/dark-mode-toggle"></script>
-<dark-mode-toggle
-      style="display: flex; justify-content: flex-end"
-      id="dark-mode-toggle-1"
-      appearance="switch"
-      dark="Dark"
-      light="Light"
-      remember="Remember this"
-></dark-mode-toggle>
+<dark-mode-toggle style="display: flex; justify-content: flex-end" id="dark-mode-toggle-1" appearance="switch" dark="Dark" light="Light" remember="Remember this"></dark-mode-toggle>
