973a6a9c9eeff679cc3637d32716a4db997bc058
Filip Troníček <filip.tronicek@seznam.cz>
2020-11-23 14:07:55 +0100

    Arrow function

--- js/file.js
+++ js/file.js
@@ -116,7 +116,7 @@ function copyTextToClipboard(text) {
     return;
   }
   navigator.clipboard.writeText(text).then(
-    function () {
+    () => {
       console.log("Async: Copying to clipboard was successful!");
       $(".copy").css("background", "#0db60d");
       setTimeout(function () {
