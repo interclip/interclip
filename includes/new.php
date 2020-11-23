1520216a86b8d45121e44092dcf275f3f26f7f7a
Filip Troníček <filip.tronicek@seznam.cz>
2020-11-23 21:47:05 +0100

    Formatting of new

--- includes/new.php
+++ includes/new.php
@@ -1,6 +1,7 @@
 <link rel="stylesheet" href="../css/new.css">
 <link rel="stylesheet" href="../css/dark.css" media="(prefers-color-scheme: dark)">
 <title> New clip - Interclip </title>
+
 <?php
 include_once "menu.php";
 ?>
@@ -8,14 +9,16 @@ include_once "menu.php";
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <?php
-if (isset($_POST['input'])) {
-  $url = $_POST['input'];
-  include_once "./components/new.php";
-}
+  if (isset($_POST['input'])) {
+    $url = $_POST['input'];
+    include_once "./components/new.php";
+  }
 ?>
+
 <script src="https://cdn.jsdelivr.net/gh/jquery/jquery@3.2.1/dist/jquery.min.js"> </script>
 <script src="https://cdn.jsdelivr.net/gh/aperta-principium/Embed/embed.js"> </script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.qrcode/1.0/jquery.qrcode.min.js"> </script>
+
 <div id="fullscreen">
   <div class="fullscreen-content">
 
