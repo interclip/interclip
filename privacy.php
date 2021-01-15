217dacbaa67c710addff86792ecd62e15f1b802c
Filip Troníček <filip.tronicek@seznam.cz>
2021-01-15 23:46:14 +0100

    Update privacy.php

--- privacy.php
+++ privacy.php
@@ -38,16 +38,12 @@
 
         <p>The personal information that you are asked to provide, and the reasons why you are asked to provide it, will be made clear to you at the point we ask you to provide your personal information.</p>
         <p>If you contact us directly, we may receive additional information about you such as your name, email address, phone number, the contents of the message and/or attachments you may send us, and any other information you may choose to provide.</p>
-        <p>When you register for an Account, we may ask for your contact information, including items such as name, company name, address, email address, and telephone number.</p>
 
         <h2>How we use your information</h2>
 
         <h2>Log Files</h2>
 
         <p>Interclip follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.</p>
-
-
-
 
         <h2>Advertising Partners Privacy Policies</h2>
 
