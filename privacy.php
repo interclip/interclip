5f533bc277a2cc2c651b789456be9a64a186f36c
Filip Troníček <filip.tronicek@seznam.cz>
2021-01-17 16:59:56 +0100

    Finally set all fonts to Palanquin

--- privacy.php
+++ privacy.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link rel="stylesheet" href="./css/dark.css" media="(prefers-color-scheme: dark)">
     <link rel="stylesheet" href="./css/new.css">
     <link rel="stylesheet" href="./css/privacy.css">
