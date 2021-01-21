103a2bb5b157b6c2ebd297623ed9c1d315ea250e
Filip Troníček <filip.tronicek@seznam.cz>
2021-01-21 17:12:13 +0100

    Remove unused sources

--- recieve.php
+++ recieve.php
@@ -43,10 +43,6 @@
             </form>
 
             <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-            <script src="https://codepen.io/electerious/pen/rroqdL"></script>
-
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-            <script src="https://codepen.io/electerious/pen/rroqdL.js"></script>
             <script type="text/javascript" src="js/recieve.js"></script>
         </div>
 
