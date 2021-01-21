17ad60f8d1d0eaa16a51bcc57c44dbc521a8a57f
Filip Troníček <filip.tronicek@seznam.cz>
2021-01-21 17:14:06 +0100

    Eqeqeq

--- js/validate.js
+++ js/validate.js
@@ -14,7 +14,7 @@ function validateForm() {
 }
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
+  if (event.target === modal) {
     modal.style.display = "none";
   }
 };
