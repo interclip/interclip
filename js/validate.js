9a69e4e1db7ad8cbd4a469e57ef6730d7345a990
Filip Troníček <filip.tronicek@seznam.cz>
2021-01-21 17:14:18 +0100

    Arrow function

--- js/validate.js
+++ js/validate.js
@@ -13,7 +13,7 @@ function validateForm() {
   }
 }
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = (event) => {
   if (event.target === modal) {
     modal.style.display = "none";
   }
