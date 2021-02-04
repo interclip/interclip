b524179b06a7575ef6b13a21c3d1b9213c841e53
Filip Troníček <filip.tronicek@seznam.cz>
2021-02-04 09:39:44 +0100

    Execute script and create database iclip

---
+++ scripts/db.sql
@@ -0,0 +1,29 @@
+/* Rate limit table */
+DROP TABLE IF EXISTS `hits`;
+CREATE TABLE `hits` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `iphash` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `date` datetime NOT NULL,
+  `operation` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4209 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/* Clips table */
+DROP TABLE IF EXISTS `userurl`;
+CREATE TABLE `userurl` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `usr` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `url` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `date` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `expires` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1182 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+``` 
+
+### Setting up MySQL cron jobs
+```sql
+/* Delete expired clips (runs every hour) */
+CREATE EVENT `clean_expired` ON SCHEDULE EVERY 1 HOUR STARTS '2021-02-01 13:39:14' ON COMPLETION NOT PRESERVE ENABLE DO DELETE FROM userurl WHERE expires < CURDATE();
+
+/* Cleans the DB hits table with events older than 5 hours */ 
+CREATE EVENT `clean_hits` ON SCHEDULE EVERY 1 DAY STARTS '2014-01-18 00:00:00' ON COMPLETION NOT PRESERVE ENABLE DO DELETE FROM `hits` where `date` < (CURRENT_TIMESTAMP - 18000);
