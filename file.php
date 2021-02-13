862e84244a58830a35f75acbef64e83bda0652e1
Filip Troníček <filip.tronicek@seznam.cz>
2021-02-13 19:44:46 +0000

    File uploads disabled

--- file.php
+++ file.php
@@ -14,37 +14,19 @@
     <link rel="stylesheet" href="./css/menu.css">
 </head>
 <?php
-include("includes/menu.php");
+    include("includes/menu.php");
 ?>
 
 <body>
-    <span id="content"> </span>
-    <div class="title">
-        <h1>Upload a file to Interclip</h1>
-        <p class="note">Upload any file type that's under 100MB.</p>
-        <div class="demo-droppable">
-            <p>Drag files here or click to upload</p>
-        </div>
-                
-        <div id="modal" class="modal">
+    <?php 
+        $fileUpload = false;
 
-        <!-- Modal content -->
-        <div class="modal-content">
-            <p><img height="250" src="css/loading-spin.svg" alt=""></p>
-        </div>
-
-        </div>
-        <div class="output"></div>
-        <span id="code" class="code"></span>
-        <br>
-        <button id="copy" class="copy">Copy code</button>
-    </div>
-
-    <script>
-        clickEnabled = true;
-    </script>
-    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-    <script src="js/file.js"></script>
+        if ($fileUpload) {
+            include "includes/components/file-ok.php";
+        } else {
+            include "includes/components/file-disabled.php";
+        }
+    ?>
 </body>
 
 </html>
---
+++ includes/components/file-disabled.php
@@ -0,0 +1,6 @@
+<span id="content"> </span>
+<div class="title">
+    <h1>Upload a file to Interclip</h1>
+    <p class="note">Sorry, file uploads to Interclip are disabled at this time, we're working on a fix. See <a href="https://github.com/aperta-principium/Interclip/issues/59" target="_blank" rel="noreferrer nofollow">#59</a> on why</p>
+
+</div>
