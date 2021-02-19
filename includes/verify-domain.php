f74abe05b05b58432ab2cc50c6f23d742e81a4c5
Filip Troníček <filip.tronicek@seznam.cz>
2021-02-19 14:18:00 +0000

    Use provided domain from request

--- includes/verify-domain.php
+++ includes/verify-domain.php
@@ -13,11 +13,14 @@ use Iodev\Whois\Factory;
 $whois = Factory::get()->createWhois();
 
 if (isset($_GET['domain'])) {
+
+    $domain = $_GET['domain'];
+
     // Checking availability
-    if ($whois->isDomainAvailable("trtgnck.dev")) {
-        echo json_encode(['status' => 'success', 'result' => ['registered' => false, 'took' => microtime(true) - $start]]);
+    if ($whois->isDomainAvailable($domain)) {
+        echo json_encode(['status' => 'success', 'result' => ['registered' => false, 'domain' => $domain, 'took' => microtime(true) - $start]]);
     } else {
-        echo json_encode(['status' => 'success', 'result' => ['registered' => true, 'took' => microtime(true) - $start]]);
+        echo json_encode(['status' => 'success', 'result' => ['registered' => true, 'domain' => $domain, 'took' => microtime(true) - $start]]);
     }
 } else {
     echo json_encode(['status' => 'error', 'result' => 'You must provide a URL parameter in the request.']);
