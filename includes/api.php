099c9252ca730f78464fea9ae035bee57f0e925c
Filip Troníček <filip.tronicek@seznam.cz>
2021-02-22 16:07:45 +0000

    Add domain verification to the API

--- includes/api.php
+++ includes/api.php
@@ -5,18 +5,27 @@ header('Access-Control-Allow-Origin: *');
 
 function writeDb($url)
 {
-  if (isset($url)) {
-    if (filter_var($url, FILTER_VALIDATE_URL)) {
-      include "db.php";
-      include_once "components/new.php";
-    }
-    if(isset($usr) && $usr != null) {
-      echo json_encode(['status' => 'success', 'result' => $usr]);
-    } else {
-      http_response_code(400);
-      echo json_encode(['status' => 'error', 'result' => 'invalid URL specified']);
-    }
+  if (filter_var($url, FILTER_VALIDATE_URL)) {
+    include "db.php";
+    include_once "components/new.php";
+  }
+  
+  $createArray = createClip($url);
+  $usr = $createArray[0];
+  $err = $createArray[1];
 
+  if (isset($url)) {
+    if ($err === "") {
+      if (isset($usr) && $usr != null) {
+        echo json_encode(['status' => 'success', 'result' => $usr]);
+      } else {
+        http_response_code(400);
+        echo json_encode(['status' => 'error', 'result' => 'invalid URL specified']);
+      }
+    } else {
+      http_response_code(503);
+      echo json_encode(['status' => 'error', 'result' => $err]);
+    }
   } else {
     http_response_code(404);
     echo json_encode(['status' => 'error', 'result' => 'no URL provided']);
