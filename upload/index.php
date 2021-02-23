4423d9758cbded598353f311a1572ca6fb6e4e42
Filip Troníček <filip.tronicek@seznam.cz>
2021-02-23 17:14:22 +0100

    Set ini val

--- upload/index.php
+++ upload/index.php
@@ -5,6 +5,7 @@
 </head>
 <body>
   <form enctype="multipart/form-data" action="upload.php" method="POST">
+  <form enctype="multipart/form-data" action="index.php" method="POST">
     <p>Upload your file</p>
     <input type="file" name="uploaded_file"></input><br />
     <input type="submit" value="Upload"></input>
@@ -12,6 +13,10 @@
 </body>
 </html>
 <?php 
+
+    ini_set("post_max_size", "10G");
+    ini_set("upload_max_filesize", "10G");
+
 
   if(!empty($_FILES['uploaded_file']))
   {
@@ -26,8 +31,7 @@
     $path = $path . $id . "." . $ext;
 
     if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
-      echo "The file ".  basename( $_FILES['uploaded_file']['name']). 
-      " has been uploaded";
+      echo "The file ".  basename( $_FILES['uploaded_file']['name']). " has been uploaded";
     } else{
         echo "There was an error uploading the file, please try again!";
     }
