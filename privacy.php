77976e1b9c16b083d47fe11eecfa567b933b2170
Filip Troníček <filip.tronicek@seznam.cz>
2021-02-23 14:36:42 +0100

    Update privacy policy

--- privacy.php
+++ privacy.php
@@ -42,21 +42,7 @@
 
         <h2>Log Files</h2>
 
-        <p>Interclip follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.</p>
-
-        <h2>Advertising Partners Privacy Policies</h2>
-
-        <P>You may consult this list to find the Privacy Policy for each of the advertising partners of Interclip.</p>
-
-        <p>Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Interclip, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.</p>
-
-        <p>Note that Interclip has no access to or control over these cookies that are used by third-party advertisers.</p>
-
-        <h2>Third Party Privacy Policies</h2>
-
-        <p>Interclip's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options. </p>
-
-        <p>You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites.</p>
+        <p>Interclip follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site and gathering demographic information.</p>
 
         <h2>CCPA Privacy Rights (Do Not Sell My Personal Information)</h2>
 
