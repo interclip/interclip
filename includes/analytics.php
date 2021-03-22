9528be7ccbd948a4a28eb9a3ff6de106e11a14f4
Filip Troníček <filip.tronicek@seznam.cz>
2021-03-22 15:23:53 +0000

    Cloudflare analytics

--- includes/analytics.php
+++ includes/analytics.php
@@ -1,18 +1,3 @@
-<script type="text/javascript">
-var _paq = window._paq || [];
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-(function() {
-    var u = "//nfogix.com/";
-    _paq.push(['setTrackerUrl', u + 'api/track']);
-    _paq.push(['setSiteId', '2accc6dc-f7cc-482c-8403-3687eac862cb']);
-    var d = document,
-        g = d.createElement('script'),
-        s = d.getElementsByTagName('script')[0];
-    g.type = 'text/javascript';
-    g.async = true;
-    g.defer = true;
-    g.src = u + 'js/nfogix.min.js';
-    s.parentNode.insertBefore(g, s);
-})();
-</script>
+<!-- Cloudflare Web Analytics -->
+<script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "0bf18f78dfbd40b2953f59d5d0034af2"}'></script>
+<!-- End Cloudflare Web Analytics -->
