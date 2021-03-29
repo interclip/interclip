c37fa48678fc4edad0604e7d5298015af7af2f8e
Filip Troníček <filip.tronicek@seznam.cz>
2021-03-29 21:06:29 +0000

    Nonce verification

--- includes/anti-csrf.php
+++ includes/anti-csrf.php
@@ -13,15 +13,23 @@ function validate()
 {
     // Start the session
     session_start();
-
-    if ($_SESSION['token'] === $_POST['token']) {
-        if (time() >= $_SESSION['token-expire']) {
-            exit("Token expired. Please reload go back and repeat the action.");
+    if (isset($_POST['token'])) {
+        if ($_SESSION['token'] === $_POST['token']) {
+            if (time() >= $_SESSION['token-expire']) {
+                exit("Token expired. Please reload go back and repeat the action.");
+            } else {
+                unset($_SESSION['token']);
+                unset($_SESSION['token-expire']);
+            }
         } else {
-            unset($_SESSION['token']);
-            unset($_SESSION['token-expire']);
+            if (isset($_SESSION['token'])) {
+                exit("Honestly, no idea what went wrong, but it's not my fault, I promise.");
+            }
+            http_response_code(400);
+            exit("INVALID ANTI-CSRF TOKEN, sending you back. <script>location.href='../';</script>");
         }
     } else {
-        exit("INVALID ANTI-CSRF TOKEN, sending you back. <script>location.href='../';</script>");
+        http_response_code(400);
+        exit("All went well... just kidding, something went wrong, and it looks like it's something on your end. Either you're here since like 1995 or yoU're trying to steal our requests, either way, don't do that!");  
     }
 }
