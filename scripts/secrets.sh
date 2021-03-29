8696f7ed6d8e12cb96dce931a36ccb2cc3b19ac0
Filip Troníček <filip.tronicek@seznam.cz>
2021-03-29 08:56:12 +0000

    GitPod generate .env file

--- scripts/secrets.sh
+++ scripts/secrets.sh
@@ -1,13 +1,3 @@
-# Generate salt file
+# Copy the sample GitPod .env
 
-salt=$(date +"%s")
-echo "<?php" > includes/salt.php
-echo '$salt = "'$salt'";' >> includes/salt.php
-
-# Generate DB file
-
-echo "<?php" > includes/db.php
-echo '$servername = "localhost:3306";'>> includes/db.php
-echo '$DBName = "iclip";' >> includes/db.php
-echo '$username = "root";' >> includes/db.php
-echo '$password = "";' >> includes/db.php
+cp scripts/.gitpod.env .env
