cad075a59f9e65b73e43e595f208004a601340a8
Filip Troníček <filip.tronicek@seznam.cz>
2021-04-02 14:57:14 +0000

    Align avatar to vertical center

--- about.php
+++ about.php
@@ -59,7 +59,12 @@
 
     <div class='madeBy center'>
         made with ❤️  <i class="icon ion-heart"></i> and a little bit of code by &nbsp;
-        <span><a href="https://github.com/filiptronicek"><img src="https://github.com/filiptronicek.png"></a></span></div>
+        <span>
+            <a href="https://github.com/filiptronicek">
+                <img src="https://github.com/filiptronicek.png">
+            </a>
+        </span>
+    </div>
 
 
 </body>
