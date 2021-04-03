75226691ec90123c82812119e8076ab96ae16444
Filip Troníček <filip.tronicek@seznam.cz>
2021-04-03 19:53:28 +0200

    Create facts.js

---
+++ api/facts.js
@@ -0,0 +1,17 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+/**
+ * Respond to the request
+ * @param {Request} request
+ */
+async function handleRequest(request) {
+  const res = await fetch(url); // Fetch the text file
+  const result = await res.text(); // Load the resulting text
+
+  const messageArray = result.split("\n"); // Split the text lines to an arrray
+  const randomItem = messageArray[Math.floor(Math.random() * messageArray.length)]; // Get a random item from the array
+
+  return new Response(randomItem, {headers: {"Access-Control-Allow-Origin": "*"}, status: 200})
+}
