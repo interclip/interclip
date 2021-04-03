4e01399c41c973e5fd9f5b94237429e8da89db3e
Filip Troníček <filip.tronicek@seznam.cz>
2021-04-03 10:42:20 +0000

    Rename files to adjust for the typo

--- receive.php
+++ receive.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>receive link | Interclip</title>
+    <title>Receive link | Interclip</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
     <link href="css/button.css" rel="stylesheet" />
     <link rel="stylesheet" type="text/css" href="css/receive.css">
@@ -27,7 +27,7 @@
                 <div class="full-screen">
 
                     <div class="input-wrap">
-                        <p class="title">receive link</p>
+                        <p class="title">Receive link</p>
                         <input type="hidden" name="token" value="<?= $_SESSION['token'] ?>" />
                         <input type="text" name="user" minlength="5" maxlength="5" id="code" placeholder="h&amp;$h">
                         <br>
