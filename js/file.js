013a2cebc57b36d61d797129f415e21347bf8a9a
Filip Troníček <filip.tronicek@seznam.cz>
2021-04-03 17:26:11 +0000

    Code style

--- js/file.js
+++ js/file.js
@@ -2,9 +2,9 @@ const modal = document.getElementById("modal");
 const output = document.querySelector(".output");
 const fact = document.getElementById("fact");
 
-fetch("https://interclips.filiptronicek.workers.dev/").then(res => res.text()).then(res => {
+fetch("https://interclips.filiptronicek.workers.dev/").then((res) => res.text()).then((res) => {
   fact.innerText = res;
-})
+});
 
 const fileSizeLimitInMegabytes = 100;
 const fileSizeLimitInBytes = fileSizeLimitInMegabytes * 1048576;
