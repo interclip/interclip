c203a59d1e01034e99d5cf104ee9625b4058d75d
Filip Troníček <filip.tronicek@seznam.cz>
2021-04-10 10:42:28 +0000

    Property shorthand

--- js/new.js
+++ js/new.js
@@ -7,9 +7,9 @@ if (navigator.share) {
 function share() {
     if (navigator.share) {
         navigator.share({
-            url: url,
-            title: 'Interclip',
-            text: 'A URL shared with Interclip',
+            url,
+            title: "Interclip",
+            text: "A URL shared with Interclip",
         })
             .catch((error) => console.log('Sharing failed', error));
     } else {
