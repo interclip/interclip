3a33ef72ac37f63e84c7797f65e6993b86ccf7e0
Filip Troníček <filip.tronicek@seznam.cz>
2021-04-13 07:29:53 +0000

    Don't get stuck in a redirect loop in login

--- login.php
+++ login.php
@@ -3,9 +3,9 @@ require "vendor/autoload.php";
 require "includes/header.php";
 
 // Do we have an authenticated session available?
-if ($user = $auth0->getUser()) {
+if ($auth0->getUser()) {
     // Output the authenticated user
-    header("Location: " . ROOT);
+    header("Location: " . ROOT . "/");
 } else {
     // No session was available, so redirect to Universal Login page
     $auth0->login();
