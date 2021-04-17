500e352322a71588b35ce731dfe1219115cd169f
Filip Troníček <filip.tronicek@seznam.cz>
2021-04-17 15:56:04 +0000

    SW in menu.js

--- js/menu.js
+++ js/menu.js
@@ -181,3 +181,14 @@ updateMenu();
 window.addEventListener("load", () => {
     showPaintTimings();
 });
+
+// Check that service workers are supported
+if ('serviceWorker' in navigator) {
+    // Use the window load event to keep the page load performant
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js');
+    });
+  } else {
+      console.log(navigator.serviceWorker)
+  }
+  
