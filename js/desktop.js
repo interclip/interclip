8511e6b8cbf108bf96d2ec12a080c7edaf3d9dba
Filip Troníček <filip.tronicek@seznam.cz>
2021-04-17 20:27:48 +0000

    refactor: Remove jQuery get

--- js/desktop.js
+++ js/desktop.js
@@ -2,19 +2,15 @@ const downloadsUrl =
   "https://github.com/aperta-principium/Interclip-desktop/releases/latest/download/Interclip-";
 
 function download(os) {
-  $.get(
-    "https://api.github.com/repos/aperta-principium/Interclip-desktop/releases",
-    {},
-    function(data) {
-      if (os === "Linux" || os === "Ubuntu") {
-        location.href =
-          `${downloadsUrl + data[0].tag_name.replace("v", "")}.AppImage`;
-      } else if (os === "Windows") {
-        location.href = `${downloadsUrl}install.exe`;
-      } else if (os === "Macos") {
-        location.href =
-          `${downloadsUrl + data[0].tag_name.replace("v", "")}.dmg`;
-      }
+  fetch("https://api.github.com/repos/aperta-principium/Interclip-desktop/releases").then((r) => r.json()).then((data) => {
+    if (os === "Linux" || os === "Ubuntu") {
+      location.href =
+        `${downloadsUrl + data[0].tag_name.replace("v", "")}.AppImage`;
+    } else if (os === "Windows") {
+      location.href = `${downloadsUrl}install.exe`;
+    } else if (os === "Macos") {
+      location.href =
+        `${downloadsUrl + data[0].tag_name.replace("v", "")}.dmg`;
     }
-  );
+  });
 }
