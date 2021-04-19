db8d2be68853b869d1dfeb55d9d4434c1b58a405
Filip Troníček <filip.tronicek@seznam.cz>
2021-04-19 18:30:50 +0200

    isMobile() edge cases
    If the User Agent is not set, simply return false

--- index.php
+++ index.php
@@ -19,7 +19,11 @@
     <?php
 
     function isMobile() {
-        return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
+        if (!empty($_SERVER["HTTP_USER_AGENT"])) {
+            return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
+        } else {
+            return false;
+        }
     }
 
     include "includes/anti-csrf.php";
