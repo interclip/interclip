972b6a0da65b93baff725273ad7b1dec0cf6d08f
Filip Troníček <filip.tronicek@seznam.cz>
2021-04-24 14:20:13 +0200

    Check format of branch name

--- includes/change-branch.php
+++ includes/change-branch.php
@@ -8,8 +8,12 @@ header('Content-Type: application/json');
 if ($user !== false) {
     if (!empty($_GET['branch'])) {
         $targetBranch = $_GET['branch'];
-        exec("git checkout $targetBranch");
-        echo json_encode(["status" => "success", "result" => "Executed checkout to $targetBranch"]);
+        if (preg_match("/^([a-z]|[0-9]|[-\/]){1,255}$/", $targetBranch)) { // https://regexr.com/5rgr0
+            exec("git checkout $targetBranch");
+            echo json_encode(["status" => "success", "result" => "Executed checkout to $targetBranch"]);
+        } else {
+            echo json_encode(["status" => "error", "result" => "Yikes.. that would be an invalid git branch format"]);
+        }
     } else {
         echo json_encode(["status" => "error", "result" => "You need to provide a branch name :("]);
     }
