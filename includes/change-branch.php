c9cd71c3d46b244ae56e73ffae3508f7274e7b1d
Filip Troníček <filip.tronicek@seznam.cz>
2021-04-24 14:28:47 +0200

    Can't change branches while in production mode.

--- includes/change-branch.php
+++ includes/change-branch.php
@@ -5,18 +5,22 @@ include_once "lib/auth.php";
 
 header('Content-Type: application/json');
 
-if ($user !== false) {
-    if (!empty($_GET['branch'])) {
-        $targetBranch = $_GET['branch'];
-        if (preg_match("/^([a-z]|[0-9]|[-\/]){1,255}$/", $targetBranch)) { // https://regexr.com/5rgr0
-            exec("git checkout $targetBranch");
-            echo json_encode(["status" => "success", "result" => "Executed checkout to $targetBranch"]);
+if ($_ENV['ENVIRONMENT'] !== "production") {
+    if ($user !== false) {
+        if (!empty($_GET['branch'])) {
+            $targetBranch = $_GET['branch'];
+            if (preg_match("/^([a-z]|[0-9]|[-\/]){1,255}$/", $targetBranch)) { // https://regexr.com/5rgr0
+                exec("git checkout $targetBranch");
+                echo json_encode(["status" => "success", "result" => "Executed checkout to $targetBranch"]);
+            } else {
+                echo json_encode(["status" => "error", "result" => "Yikes.. that would be an invalid git branch format"]);
+            }
         } else {
-            echo json_encode(["status" => "error", "result" => "Yikes.. that would be an invalid git branch format"]);
+            echo json_encode(["status" => "error", "result" => "You need to provide a branch name :("]);
         }
     } else {
-        echo json_encode(["status" => "error", "result" => "You need to provide a branch name :("]);
+        echo json_encode(["status" => "error", "result" => "Not logged in"]);
     }
 } else {
-    echo json_encode(["status" => "error", "result" => "Not logged in"]);
+    echo json_encode(["status" => "error", "result" => "Can't change branches while in production mode."]);
 }
