0da2dc25fa659e3d34e7990fded736786011602a
Filip Troníček <filip.tronicek@seznam.cz>
2021-04-25 11:34:55 +0200

    perf: Solve QR codes once and for all (#106)
    * Solve QR codes once and for all
    
    With vanilla JS
    
    * Fix overflow on new clip page

--- includes/components/html/new/ok-msg.php
+++ includes/components/html/new/ok-msg.php
@@ -1,15 +1,15 @@
 <?php
-    $createArray = createClip($url);
-    $usr = $createArray[0];
-    $err = $createArray[1];
+$createArray = createClip($url);
+$usr = $createArray[0];
+$err = $createArray[1];
 ?>
 
-<?php if ($err === ""): ?>
+<?php if ($err === "") : ?>
     <p><span id="url" class="url"><?php echo $url ?> </span><br><br> was saved as</p>
     <h1 class="usrCode"><?php echo $usr ?></h1>
     <div id="embed"> </div>
     <div id="qrcode"></div>
-<?php else: ?>
+<?php else : ?>
     <p><span id="url" class="url"><?php echo $url ?> </span><br></p>
     <h1 class="usrCode"><?php echo $err ?></h1>
 <?php endif; ?>
@@ -18,7 +18,7 @@
 <script type="module">
     /* MIT  Copyright (c) Feross Aboukhadijeh */
 
-    function colorSchemeChange (onChange) {
+    function colorSchemeChange(onChange) {
         const media = window.matchMedia('(prefers-color-scheme: dark)')
 
         handleChange()
@@ -29,10 +29,10 @@
             media.addListener(handleChange)
         }
 
-        function handleChange () {
-            const scheme = media.matches
-            ? 'dark'
-            : 'light'
+        function handleChange() {
+            const scheme = media.matches ?
+                'dark' :
+                'light'
             onChange(scheme)
         }
     }
@@ -40,51 +40,42 @@
     /* End of copyrighted code, code from https://github.com/feross/color-scheme-change */
 
     const update = (scheme) => {
-        options = {
-            text: "https://interclip.app/<?php echo $usr ?>",
-            background: scheme === 'dark' ? "#444444" : "#ff9800",
-            foreground: scheme === 'dark' ? "#e4e4e4" : "#000000",
+
+        const style = window
+            .getComputedStyle(document.documentElement)
+            .getPropertyValue('content')
+            .replace(/"/g, '')
+
+        if (scheme === null || scheme === "system") {
+            scheme = style;
         }
+
         document.getElementById('qrcode').innerHTML = "";
-        $('#qrcode').qrcode(options);
+        const qr = QRCode.generateSVG("https://interclip.app/<?php echo $usr ?>", {
+            ecclevel: "M",
+            fillcolor: scheme === 'dark' ? "#444444" : "#ff9800",
+            textcolor: scheme === 'dark' ? "#e4e4e4" : "#000000",
+            margin: 0,
+            modulesize: 4
+        });
+        document.getElementById("qrcode").appendChild(qr);
     }
-
-
-    const style = window
-        .getComputedStyle(document.documentElement)
-        .getPropertyValue('content')
-        .replace(/"/g, '')
 
     const computedStyle = localStorage.getItem("dark-mode-toggle");
-    
+
     let options;
 
-    if (computedStyle === null || computedStyle === "light") {
-        options = {
-            text: "https://https://interclip.app/<?php echo $usr ?>",
-            background: "#ff9800",
-            foreground: "#000000",
-        }
-    } else if (computedStyle == "dark") {
-        options = {
-            text: "https://interclip.app/<?php echo $usr ?>",
-            background: "#444444",
-            foreground: "#e4e4e4",
-        }
-    }
-    $('#qrcode').qrcode(options);
+    update(computedStyle);
 
     colorSchemeChange(colorScheme => {
         const switcherScheme = localStorage.getItem("dark-mode-toggle");
         update(switcherScheme ? colorScheme : switcherScheme);
-    })
-
-    document.querySelector("#dark-mode-toggle-1").addEventListener('click', function() {
-        setTimeout(() => {
-            update(localStorage.getItem("dark-mode-toggle"));
-        }, 20);
     });
 
-    update(localStorage.getItem("dark-mode-toggle")) || style;
+    const toggle = document.getElementById("slct");
+
+    toggle.addEventListener('change', () => {
+        update(toggle.value);
+    });
 
 </script>
