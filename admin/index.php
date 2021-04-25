32cd2140ca8a3bdde11a4ad994bce8b862facf99
Filip Troníček <filip.tronicek@seznam.cz>
2021-04-25 17:38:02 +0000

    HTML layout

---
+++ admin/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard | Interclip</title>
+    <link rel="stylesheet" href="https://unpkg.com/mvp.css">
+</head>
+
+<body>
+    <main>
+        <section id="intro">
+            <header>
+                <h2>Welcome to the Interclip admin dashboard!<sup>BETA</sup></h2>
+            </header>
+            <aside>
+                <img alt="An image of HTML, CSS and JavaScript" src="https://files.catbox.moe/fbdwm0.svg" height="150" />
+                <h3>Interclip files</h3>
+                <p>Total files: <strong>56</strong></p>
+                <p>Total size: <strong>5GB</strong></p>
+            </aside>
+            <aside>
+                <img alt="An image of HTML, CSS and JavaScript" src="https://files.catbox.moe/1ahd5p.svg" height="150" />
+                <h3>Server metrics</h3>
+                <p>Server load: <strong>0.05</strong></p>
+                <p>Storage: <strong>9GB/20GB</strong></p>
+                <p>Uptime: <strong>55 day</strong></p>
+            </aside>
+            <aside>
+                <img alt="An image of HTML, CSS and JavaScript" src="https://files.catbox.moe/hggz12.svg" height="150" />
+                <h3>Runtime details</h3>
+                <p>Used memory: <strong>250KB</strong></p>
+                <p>PHP version: <strong>8.0.2</strong></p>
+            </aside>
+        </section>
+    </main>
+</body>
+
+</html>
