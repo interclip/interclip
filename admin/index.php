6d5b81c3af1b8fbbbf01ebfe4aea2454c70e9502
Filip Troníček <filip.tronicek@seznam.cz>
2021-04-25 17:56:56 +0000

    Add some stats dynamically

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<?php
+
+include_once "../includes/lib/init.php";
+include_once "../includes/lib/auth.php";
+
+include_once "../includes/lib/sentry.php";
+include_once "../includes/lib/functions.php";
+
+if (!$isWindows) {
+    $systemLoad = sys_getloadavg()[0];
+    $uptime = explode(',', explode(' up ', shell_exec('uptime'))[1])[0];
+} else {
+    $systemLoad = "n/a";
+    $uptime = "n/a";
+}
+
+?>
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,7 +31,8 @@
     <main>
         <section id="intro">
             <header>
-                <h2>Welcome to the Interclip admin dashboard!<sup>BETA</sup></h2>
+                <h1>Hi, <?php echo $user["name"] ? $user['name'] : $user["nickname"] ?></h1>
+                <p>Welcome to the Interclip admin dashboard!<sup>BETA</sup></p>
             </header>
             <aside>
                 <img alt="An image of HTML, CSS and JavaScript" src="https://files.catbox.moe/fbdwm0.svg" height="150" />
@@ -24,15 +43,15 @@
             <aside>
                 <img alt="An image of HTML, CSS and JavaScript" src="https://files.catbox.moe/1ahd5p.svg" height="150" />
                 <h3>Server metrics</h3>
-                <p>Server load: <strong>0.05</strong></p>
-                <p>Storage: <strong>9GB/20GB</strong></p>
-                <p>Uptime: <strong>55 day</strong></p>
+                <p>Server load: <strong><?php echo $systemLoad ?></strong></p>
+                <p>Storage: <strong><?php echo (formatBytes(disk_total_space('/') - disk_free_space('/'))) . "/" . (formatBytes(disk_total_space('/'))) ?></strong></p>
+                <p>Uptime: <strong><?php echo $uptime ?></strong></p>
             </aside>
             <aside>
                 <img alt="An image of HTML, CSS and JavaScript" src="https://files.catbox.moe/hggz12.svg" height="150" />
                 <h3>Runtime details</h3>
-                <p>Used memory: <strong>250KB</strong></p>
-                <p>PHP version: <strong>8.0.2</strong></p>
+                <p>Used memory: <strong><?php echo formatBytes(memory_get_usage()) ?></strong></p>
+                <p>PHP version: <strong><?php echo phpversion(); ?></strong></p>
             </aside>
         </section>
     </main>
