332df2153c028204af477f95262490bff8d742d5
Filip Troníček <filip.tronicek@seznam.cz>
2021-04-26 13:50:19 +0000

    Format functions

--- includes/lib/functions.php
+++ includes/lib/functions.php
@@ -114,18 +114,18 @@ function getOSInformation()
         return null;
     }
 
-    $os         = shell_exec('cat /etc/os-release');
-    $listIds    = preg_match_all('/.*=/', $os, $matchListIds);
+    $osInfo = shell_exec('cat /etc/os-release');
+    $listIds = preg_match_all('/.*=/', $osInfo, $matchListIds);
     $listIds    = $matchListIds[0];
 
-    $listVal    = preg_match_all('/=.*/', $os, $matchListVal);
-    $listVal    = $matchListVal[0];
+    $listVal = preg_match_all('/=.*/', $osInfo, $matchListVal);
+    $listVal = $matchListVal[0];
 
-    array_walk($listIds, function(&$v, $k){
+    array_walk($listIds, function (&$v, $k) {
         $v = strtolower(str_replace('=', '', $v));
     });
 
-    array_walk($listVal, function(&$v, $k){
+    array_walk($listVal, function (&$v, $k) {
         $v = preg_replace('/=|"/', '', $v);
     });
 
