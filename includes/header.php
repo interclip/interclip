cc1cc1fd9161c62b4a5877da6743882ea7e113cd
Filip Troníček <filip.tronicek@seznam.cz>
2021-04-28 22:19:41 +0200

    fix: Prevent undefined $isWindows

--- includes/header.php
+++ includes/header.php
@@ -1,21 +1,29 @@
 <?php
-    /* Important variables */
-    include_once "lib/init.php";
 
-    /* Sentry */
-    include_once "lib/sentry.php";
+/* Important variables */
+include_once "lib/init.php";
 
-    /* Headers */
-    include_once "lib/headers.php";
+/* Sentry */
+include_once "lib/sentry.php";
 
-    /* Authentication */
-    include_once "lib/auth.php";
+/* Headers */
+include_once "lib/headers.php";
 
-    /* Meta tags */
-    include_once "components/html/meta-tags.php";
+/* Authentication */
+include_once "lib/auth.php";
 
-    if ($_ENV['ENVIRONMENT'] === "production") {
-        include_once 'components/html/analytics.php';
-    }
-      
-?>
+/* Meta tags */
+include_once "components/html/meta-tags.php";
+
+if ($_ENV['ENVIRONMENT'] === "production") {
+    include_once 'components/html/analytics.php';
+}
+
+//By default, we assume that PHP is NOT running on windows.
+$isWindows = false;
+
+//If the first three characters PHP_OS are equal to "WIN",
+//then PHP is running on a Windows operating system.
+if (strcasecmp(substr(PHP_OS, 0, 3), 'WIN') === 0) {
+    $isWindows = true;
+}
--- includes/menu.php
+++ includes/menu.php
@@ -13,15 +13,6 @@ $pages = array(
   'about' => ['about', 'About']
 );
 
-//By default, we assume that PHP is NOT running on windows.
-$isWindows = false;
-
-//If the first three characters PHP_OS are equal to "WIN",
-//then PHP is running on a Windows operating system.
-if (strcasecmp(substr(PHP_OS, 0, 3), 'WIN') === 0) {
-  $isWindows = true;
-}
-
 include_once "components/html/adminbar.php";
 
 // Render the menu
