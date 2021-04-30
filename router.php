5e10a95ac7ecee450175741c25cc46c278bb368c
Filip Troníček <filip.tronicek@seznam.cz>
2021-04-30 11:05:07 +0000

    Comments

--- router.php
+++ router.php
@@ -4,13 +4,10 @@ include_once "includes/lib/init.php";
 
 use Pecee\SimpleRouter\SimpleRouter;
 
+/* Dynamic pages */
+
 SimpleRouter::get('/', function() {
     $content = include_once "public/index.php";
-    return $content;
-});
-
-SimpleRouter::get('/file/', function() {
-    $content = include_once "public/file.php";
     return $content;
 });
 
@@ -19,6 +16,18 @@ SimpleRouter::get('/receive/', function() {
     return $content;
 });
 
+SimpleRouter::get('/file/', function() {
+    $content = include_once "public/file.php";
+    return $content;
+});
+
+SimpleRouter::get('/admin/', function() {
+    $content = include_once "public/admin.php";
+    return $content;
+});
+
+/* Static pages */
+
 SimpleRouter::get('/privacy/', function() {
     $content = include_once "public/privacy.php";
     return $content;
@@ -26,11 +35,6 @@ SimpleRouter::get('/privacy/', function() {
 
 SimpleRouter::get('/about/', function() {
     $content = include_once "public/about.php";
-    return $content;
-});
-
-SimpleRouter::get('/admin/', function() {
-    $content = include_once "public/admin.php";
     return $content;
 });
 
