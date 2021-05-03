efe0906d12564f7f693797a83b0ec21c4f5856b7
Filip Troníček <filip.tronicek@seznam.cz>
2021-05-03 21:57:15 +0200

    Add decryption

--- includes/components/crypt.php
+++ includes/components/crypt.php
@@ -6,5 +6,6 @@ $iv_length = openssl_cipher_iv_length($method);
 $iv = openssl_random_pseudo_bytes($iv_length);
 
 $encrypted_message = openssl_encrypt($message_to_encrypt, $method, $secret_key, 0, $iv);
+$decrypted_message = openssl_decrypt($encrypted_message, $method, $secret_key, 0, $iv);
 
-echo "Original: {$message_to_encrypt}, encrypted: {$encrypted_message}";
+echo "Original: {$message_to_encrypt}\nEncrypted: {$encrypted_message}.\nDecrypted: {$decrypted_message}";
