55a52efc21c013e2583d0062a3878ebdb79b5132
Filip Troníček <filip.tronicek@seznam.cz>
2021-05-03 17:04:58 +0200

    fix: Move the Windows detection
    In the admin bar, the code couldn't make sense of whether it was running on Windows or not.

--- includes/header.php
+++ includes/header.php
@@ -18,12 +18,3 @@ include_once "components/html/meta-tags.php";
 if ($_ENV['ENVIRONMENT'] === "production") {
     include_once 'components/html/analytics.php';
 }
-
-//By default, we assume that PHP is NOT running on windows.
-$isWindows = false;
-
-//If the first three characters PHP_OS are equal to "WIN",
-//then PHP is running on a Windows operating system.
-if (strcasecmp(substr(PHP_OS, 0, 3), 'WIN') === 0) {
-    $isWindows = true;
-}
--- includes/lib/init.php
+++ includes/lib/init.php
@@ -9,3 +9,12 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__, '../../.env');
 $dotenv->safeLoad();
 
 define("ROOT", $_ENV['ROOT']);
+
+//By default, we assume that PHP is NOT running on windows.
+$isWindows = false;
+
+//If the first three characters PHP_OS are equal to "WIN",
+//then PHP is running on a Windows operating system.
+if (strcasecmp(substr(PHP_OS, 0, 3), 'WIN') === 0) {
+    $isWindows = true;
+}
