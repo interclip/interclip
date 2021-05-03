f889409d825d8c2996ef2a26078eec2c429e63c1
Filip Troníček <filip.tronicek@seznam.cz>
2021-05-03 16:52:37 +0000

    Better exception logic

--- router.php
+++ router.php
@@ -7,90 +7,92 @@ use Pecee\SimpleRouter\SimpleRouter;
 
 /* Dynamic pages */
 
-SimpleRouter::get('/', function() {
+SimpleRouter::get('/', function () {
     include_once "public/index.php";
 });
 
-SimpleRouter::get('/receive/', function() {
+SimpleRouter::get('/receive/', function () {
     include_once "public/receive.php";
 });
 
-SimpleRouter::get('/file/', function() {
+SimpleRouter::get('/file/', function () {
     include_once "public/file.php";
 });
 
-SimpleRouter::form('/upload/', function() {
+SimpleRouter::form('/upload/', function () {
     include_once "public/upload/index.php";
 });
 
-SimpleRouter::get('/admin/', function() {
+SimpleRouter::get('/admin/', function () {
     include_once "public/admin.php";
 });
 
 /* Clip manipulation */
 
-SimpleRouter::form('/get', function() {
+SimpleRouter::form('/get', function () {
     include_once "public/core/get.php";
 });
 
-SimpleRouter::form('/set', function() {
+SimpleRouter::form('/set', function () {
     include_once "public/core/set.php";
 });
 
 /* Static pages */
 
-SimpleRouter::get('/desktop/', function() {
+SimpleRouter::get('/desktop/', function () {
     include_once "public/desktop.php";
 });
 
-SimpleRouter::get('/about/', function() {
+SimpleRouter::get('/about/', function () {
     include_once "public/about.php";
 });
 
 /* Auth */
-SimpleRouter::get('/login/', function() {
+SimpleRouter::get('/login/', function () {
     include_once "public/login.php";
 });
 
-SimpleRouter::get('/logout/', function() {
+SimpleRouter::get('/logout/', function () {
     include_once "public/logout.php";
 });
 
-SimpleRouter::get('/privacy/', function() {
+SimpleRouter::get('/privacy/', function () {
     include_once "public/privacy.php";
 });
 
 /* API */
-SimpleRouter::match(['get', 'post'],'/includes/api', function() {
+SimpleRouter::match(['get', 'post'], '/includes/api', function () {
     include_once "public/api/set.php";
 });
 
-SimpleRouter::match(['get', 'post'],'/includes/get-api', function() {
+SimpleRouter::match(['get', 'post'], '/includes/get-api', function () {
     include_once "public/api/get.php";
 });
 
 /* Internal behavior */
 
-SimpleRouter::get('/staging/change-branch', function() {
+SimpleRouter::get('/staging/change-branch', function () {
     include_once "public/change-branch.php";
 });
 
 use Pecee\Http\Request;
-SimpleRouter::error(function (Request $request, \Exception $exception) {
-    $statusCode = $exception->getCode();
-    include_once "includes/error.php";
-});
 
-SimpleRouter::get('/{user_code}', function ($user_code) {
+SimpleRouter::error(function (Request $request, \Exception $exception) {
     include_once "includes/lib/functions.php";
+
+    $currURL = $request->getUrl();
+    $unRootedURL = str_replace(ROOT, "", $currURL);
+    $user_code = str_replace("/", "", $unRootedURL);
+    echo $user_code;
     include_once "includes/components/get.php";
-    
+
     if (isset($url)) {
         header("Location: $url");
     } else {
-        header("Location: ");
+        $statusCode = $exception->getCode();
+        include_once "includes/error.php";
     }
-})->where(['user_code' => '([a-z|0-9|A-Z]){5}']);
+});
 
 // Start the routing
 SimpleRouter::start();
