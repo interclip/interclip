8030532babe36c6354d5482a72f9182c0c0fbb9c
Filip Troníček <filip.tronicek@seznam.cz>
2021-05-03 19:49:53 +0000

    Sample encryption script

---
+++ includes/components/crypt.php
@@ -0,0 +1,10 @@
+<?php
+$message_to_encrypt = "https://stackoverflow.com/questions/9262109/simplest-two-way-encryption-using-php";
+$secret_key = "iosxd";
+$method = "aes256";
+$iv_length = openssl_cipher_iv_length($method);
+$iv = openssl_random_pseudo_bytes($iv_length);
+
+$encrypted_message = openssl_encrypt($message_to_encrypt, $method, $secret_key, 0, $iv);
+
+echo "Original: {$message_to_encrypt}, encrypted: {$encrypted_message}";
