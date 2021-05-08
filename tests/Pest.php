5e2c324bc9d091304620877c3cde9eebd996ce2b
Filip Troníček <filip.tronicek@seznam.cz>
2021-05-09 00:15:33 +0200

    test: Bytes format function

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,10 +1,15 @@
 <?php
 
 include 'includes/lib/init.php';
+include 'includes/lib/functions.php';
 
 it('asserts URL shortening works', function () {
     $url = 'https://interclip.app/';
     $mode = 'silent';
     include_once "./includes/components/short-api.php";
     $this->assertNotEmpty($content);
+});
+
+it('should format bytes correctly', function () {
+    $this->assertTrue(formatBytes(70656) === "69 KB");
 });
