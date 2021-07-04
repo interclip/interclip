8378c1b4a07f01fc21fb9867c2b221b811abb3c4
Filip Troníček <filip.tronicek@seznam.cz>
2021-07-04 11:43:10 +0000

    Allow Redis to not be installed

--- includes/components/redis.php
+++ includes/components/redis.php
@@ -4,8 +4,12 @@ function storeRedis($key, $value)
 {
     $redis = new Redis();
 
-    //Connecting to Redis
-    $redis->connect('localhost', 6379);
+    try {
+        //Connecting to Redis
+        $redis->connect('localhost', 6379);
+    } catch (Exception $e) {
+        return false; // Failed connecting to the server
+    }
 
     if ($redis->ping()) {
         $redisCached = $redis->get($key);
@@ -22,9 +26,13 @@ function storeRedis($key, $value)
 function getRedis($key)
 {
     $redis = new Redis();
-
-    //Connecting to Redis
-    $redis->connect('localhost', 6379);
+    
+    try {
+        //Connecting to Redis
+        $redis->connect('localhost', 6379);
+    } catch (Exception $e) {
+        return false; // Failed connecting to the server
+    }
 
     if ($redis->ping()) {
         $redisCached = $redis->get($key);
