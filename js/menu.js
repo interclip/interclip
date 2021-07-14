1da8b23adb62ddb75cb364fca3b2e79146e9e4e8
Filip Troníček <filip.tronicek@seznam.cz>
2021-07-14 21:41:16 +0200

    Format the settings menu script

--- js/menu.js
+++ js/menu.js
@@ -21,77 +21,82 @@ const systemOpt = document.getElementById("systemOption");
 
 const userAgent = navigator.userAgent.toLowerCase();
 
-const isTablet = /(ipad|tablet|(android(?!.*mobile))|(windows(?!.*phone)(.*touch))|kindle|playbook|silk|(puffin(?!.*(IP|AP|WP))))/.test(
+const isTablet =
+  /(ipad|tablet|(android(?!.*mobile))|(windows(?!.*phone)(.*touch))|kindle|playbook|silk|(puffin(?!.*(IP|AP|WP))))/.test(
     userAgent
-);
+  );
 
 if (loggedIn && isAdmin) {
-    const adminbar = document.querySelector("#adminbar");
-    adminbar.style.display = localStorage.getItem("adminbarVisible") || "flex";
+  const adminbar = document.querySelector("#adminbar");
+  adminbar.style.display = localStorage.getItem("adminbarVisible") || "flex";
 }
 
 const isPhone = !isTablet
-    ? "ontouchstart" in document.documentElement &&
+  ? "ontouchstart" in document.documentElement &&
     /mobi/i.test(navigator.userAgent)
-    : false;
+  : false;
 
 function showPaintTimings() {
-    if (!loggedIn) {
-        return null;
-    }
-    if (window.performance) {
-        const performance = window.performance;
-        const performanceEntries = performance.getEntriesByType('paint');
-        performanceEntries.forEach((performanceEntry) => {
-            if (performanceEntry.name === "first-contentful-paint") {
-                const paintTime = performanceEntry.startTime;
-                document.getElementById("load").innerText = `Paint: ${Math.round(paintTime)}ms`;
-            }
-        });
-    } else {
-        console.log('Performance timing isn\'t supported.');
-    }
+  if (!loggedIn) {
+    return null;
+  }
+  if (window.performance) {
+    const performance = window.performance;
+    const performanceEntries = performance.getEntriesByType("paint");
+    performanceEntries.forEach((performanceEntry) => {
+      if (performanceEntry.name === "first-contentful-paint") {
+        const paintTime = performanceEntry.startTime;
+        document.getElementById("load").innerText = `Paint: ${Math.round(
+          paintTime
+        )}ms`;
+      }
+    });
+  } else {
+    console.log("Performance timing isn't supported.");
+  }
 }
 
 // When the user clicks the button, open the modal
 btn.onclick = () => {
-    settingsModal.style.display = "block";
+  settingsModal.style.display = "block";
 };
 
 colorSchemePreference.addEventListener("change", function () {
-    if (this.value === "system") {
-        const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-        darkModeToggle.mode = isDark ? "dark" : "light";
-        localStorage.removeItem("dark-mode-toggle");
-    } else {
-        darkModeToggle.mode = this.value;
-        localStorage.setItem("dark-mode-toggle", this.value);
-    }
+  if (this.value === "system") {
+    const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    darkModeToggle.mode = isDark ? "dark" : "light";
+    localStorage.removeItem("dark-mode-toggle");
+  } else {
+    darkModeToggle.mode = this.value;
+    localStorage.setItem("dark-mode-toggle", this.value);
+  }
 });
 
 toggle.addEventListener("change", function () {
-    if (this.checked) {
-        localStorage.removeItem("hideHashAnimation");
-    } else {
-        localStorage.setItem("hideHashAnimation", "true");
-    }
+  if (this.checked) {
+    localStorage.removeItem("hideHashAnimation");
+  } else {
+    localStorage.setItem("hideHashAnimation", "true");
+  }
 });
 
 betaToggle.addEventListener("change", function () {
-    if (this.checked) {
-        localStorage.removeItem("hideBetaMenu");
-    } else {
-        localStorage.setItem("hideBetaMenu", "true");
-    }
-    updateMenu();
+  if (this.checked) {
+    localStorage.removeItem("hideBetaMenu");
+  } else {
+    localStorage.setItem("hideBetaMenu", "true");
+  }
+  updateMenu();
 });
 
 /* Initialization */
 
-systemOpt.innerText = systemOpt.innerText += ` ${isTablet ? "📱" : isPhone ? "📱" : "💻"
-    }`;
+systemOpt.innerText = systemOpt.innerText += ` ${
+  isTablet ? "📱" : isPhone ? "📱" : "💻"
+}`;
 
-colorSchemePreference.value = localStorage.getItem("dark-mode-toggle") || "system";
+colorSchemePreference.value =
+  localStorage.getItem("dark-mode-toggle") || "system";
 
 toggle.checked = !localStorage.getItem("hideHashAnimation");
 
@@ -99,25 +104,28 @@ betaToggle.checked = !localStorage.getItem("hideBetaMenu");
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = () => {
-    settingsModal.style.display = "none";
+  settingsModal.style.display = "none";
 };
 
 // When the user clicks anywhere outside of the modal, close it
 document.onclick = (event) => {
-    if (event.target == settingsModal) {
-        settingsModal.style.display = "none";
-    }
+  if (event.target == settingsModal) {
+    settingsModal.style.display = "none";
+  }
 };
 
 const updateMenu = () => {
-    for (const li of document.querySelectorAll("#menu li")) {
-        if (li?.children[0]?.children[0]?.classList.contains("beta")) {
-            li.style.display = localStorage.getItem("hideBetaMenu") ? "none" : "block";
-        }
+  for (const li of document.querySelectorAll("#menu li")) {
+    if (li?.children[0]?.children[0]?.classList.contains("beta")) {
+      li.style.display = localStorage.getItem("hideBetaMenu")
+        ? "none"
+        : "block";
     }
+  }
 };
 
-console.log(`%c
+console.log(
+  `%c
     ▄▄▄ ▄▄    ▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄   ▄▄▄▄▄▄▄ ▄▄▄     ▄▄▄ ▄▄▄▄▄▄▄ 
     █   █  █  █ █       █       █   ▄  █ █       █   █   █   █       █
     █   █   █▄█ █▄     ▄█    ▄▄▄█  █ █ █ █       █   █   █   █    ▄  █
@@ -125,71 +133,72 @@ console.log(`%c
     █   █  ▄    █ █   █ █    ▄▄▄█    ▄▄  █    █  █   █▄▄▄█   █    ▄▄▄█
     █   █ █ █   █ █   █ █   █▄▄▄█   █  █ █    █▄▄█       █   █   █    
     █▄▄▄█▄█  █▄▄█ █▄▄▄█ █▄▄▄▄▄▄▄█▄▄▄█  █▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄▄▄█▄▄▄█    %c ${version}`,
-    "color:#FF9800",
-    "color:green;font-weight:bold");
+  "color:#FF9800",
+  "color:green;font-weight:bold"
+);
 
 if (loggedIn && isAdmin) {
-    if (document.getElementById("adminbar").classList.contains("staging")) {
-        document.getElementById("branch-select").addEventListener("change", (e) => {
-            const targetBranch = e.target.value.replace(/\s/g, "");
-            if (targetBranch !== "-") {
-                fetch(`${root}/staging/change-branch?branch=${targetBranch}`).then((res) => res.json()).then(() => {
-                    location.reload();
-                }).catch(err => {
-                    Swal.fire(
-                        "Something\'s went wrong",
-                        err.toString(),
-                        "error"
-                    );
-                });
-            }
-        });
+  if (document.getElementById("adminbar").classList.contains("staging")) {
+    document.getElementById("branch-select").addEventListener("change", (e) => {
+      const targetBranch = e.target.value.replace(/\s/g, "");
+      if (targetBranch !== "-") {
+        fetch(`${root}/staging/change-branch?branch=${targetBranch}`)
+          .then((res) => res.json())
+          .then(() => {
+            location.reload();
+          })
+          .catch((err) => {
+            Swal.fire("Something's went wrong", err.toString(), "error");
+          });
+      }
+    });
+  }
+
+  document.addEventListener("keydown", (e) => {
+    if (e.shiftKey && e.code === "KeyB") {
+      e.preventDefault();
+      const displayStatus = adminbar.style.display === "flex" ? "none" : "flex";
+
+      let newColor = "#262626";
+      if (displayStatus === "flex") {
+        if (document.getElementById("adminbar").classList.contains("staging")) {
+          newColor = "#F15922";
+        } else {
+          newColor = "#588D6A";
+        }
+      }
+      document
+        .querySelector("meta[name=theme-color]")
+        .setAttribute("content", newColor);
+      adminbar.style.display = displayStatus;
+      localStorage.setItem("adminbarVisible", displayStatus);
     }
-
-    document.addEventListener("keydown", (e) => {
-        if (e.shiftKey && e.code === "KeyB") {
-            e.preventDefault();
-            const displayStatus = adminbar.style.display === "flex" ? "none" : "flex";
-
-            let newColor = "#262626";
-            if (displayStatus === "flex") {
-                if (document.getElementById("adminbar").classList.contains("staging")) {
-                    newColor = "#F15922";
-                } else {
-                    newColor = "#588D6A";
-                }
-            }
-            document.querySelector("meta[name=theme-color]").setAttribute("content", newColor);
-            adminbar.style.display = displayStatus;
-            localStorage.setItem("adminbarVisible", displayStatus);
-        }
-    });
-
+  });
 } else if (loggedIn && !isAdmin) {
-    document.addEventListener('keydown', (e) => {
-        e.preventDefault();
-        if (e.shiftKey && e.code === "KeyB") {
-            Swal.fire(
-                'Permission error',
-                'Yikes! It seems you have to be an admin to view the admin bar. Want to be an admin? Tweet me @filiptronicek',
-                'error'
-            );
-        }
-    });
+  document.addEventListener("keydown", (e) => {
+    e.preventDefault();
+    if (e.shiftKey && e.code === "KeyB") {
+      Swal.fire(
+        "Permission error",
+        "Yikes! It seems you have to be an admin to view the admin bar. Want to be an admin? Tweet me @filiptronicek",
+        "error"
+      );
+    }
+  });
 }
 
 removeBtn.onclick = () => {
-    localStorage.clear();
-    Swal.fire({
-        icon: 'success',
-        title: 'All local data has been deleted',
-        showConfirmButton: false,
-        timer: 1700
-    });
+  localStorage.clear();
+  Swal.fire({
+    icon: "success",
+    title: "All local data has been deleted",
+    showConfirmButton: false,
+    timer: 1700,
+  });
 };
 
 updateMenu();
 
 window.addEventListener("load", () => {
-    showPaintTimings();
+  showPaintTimings();
 });
