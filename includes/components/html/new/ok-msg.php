fee75f4a0f963af2c07098deadaa448d25a97f21
Filip Troníček <filip.tronicek@seznam.cz>
2021-07-26 12:03:47 +0000

    Copy new code to clipboard

--- includes/components/html/new/ok-msg.php
+++ includes/components/html/new/ok-msg.php
@@ -6,7 +6,12 @@ $err = $createArray[1];
 
 <?php if ($err === "") : ?>
     <p><span id="url" class="url"><?php echo $url ?> </span><br><br> was saved as</p>
-    <h1 class="usrCode"><?php echo $usr ?></h1>
+    <div id="codeSection">
+        <h1 class="usrCode"><?php echo $usr ?></h1>
+        <svg fill="none" stroke="currentColor" id="copyCode" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"></path>
+        </svg>
+    </div>
     <div id="embed"> </div>
     <div id="qrcode"></div>
 <?php else : ?>
@@ -14,8 +19,22 @@ $err = $createArray[1];
     <h1 class="usrCode"><?php echo $err ?></h1>
 <?php endif; ?>
 
-
 <script type="module">
+    const url = "<?php echo $url ?>";
+    const code = "<?php echo $usr ?>";
+
+    const copyButton = document.getElementById("copyCode");
+    copyButton.onclick = () => {
+        navigator.clipboard.writeText(code);
+        Swal.fire({
+            icon: "success",
+            timer: 1000,
+            timerProgressBar: true,
+            title: "Yay!",
+            text: "Copied to clipboard"
+        })
+    };
+
     /* MIT  Copyright (c) Feross Aboukhadijeh */
 
     function colorSchemeChange(onChange) {
@@ -51,7 +70,7 @@ $err = $createArray[1];
         }
 
         document.getElementById("qrcode").innerHTML = "";
-        const qr = QRCode.generateSVG("https://interclip.app/<?php echo $usr ?>", {
+        const qr = QRCode.generateSVG(`https://interclip.app/${code}`, {
             ecclevel: "M",
             fillcolor: scheme === 'dark' ? "#151515" : "#157EFB",
             textcolor: "#e4e4e4",
@@ -79,7 +98,6 @@ $err = $createArray[1];
     });
 
     const initialValue = localStorage.getItem("recentClips");
-    const url = "<?php echo $url ?>";
 
     if (initialValue) {
         const recentlyMadeArray = JSON.parse(initialValue);
