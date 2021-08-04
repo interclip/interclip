76497c0b5dbbaa8207598ca87436199736523b2a
Filip Troníček <filip.tronicek@seznam.cz>
2021-08-04 22:56:38 +0000

    Move around some includes

--- public/core/set.php
+++ public/core/set.php
@@ -30,12 +30,44 @@
     <div class="fullscreen-content">
 
       <div class="title">
-        <?php
-        if (isset($_POST['input']))
-          include_once "includes/components/html/new/ok-msg.php";
-        else
-          include_once "includes/components/html/new/ok-msg.php";
-        ?>
+        <?php if (!isset($_POST['input'])) : ?>
+          <h1 class="errheader">
+            <span>4</span>&nbsp;
+            <span>0</span>&nbsp;
+            <span>0</span>
+          </h1>
+          <br>
+          <span id="errcode">
+            bad request
+          </span>
+        <?php else : ?>
+          <?php
+          $createArray = createClip($url);
+          $usr = $createArray[0];
+          $err = $createArray[1];
+          ?>
+
+          <?php if ($err === "") : ?>
+            <p><span id="url" class="url"><?php echo $url ?> </span><br><br> was saved as</p>
+            <div id="codeSection">
+              <h1 class="usrCode"><?php echo $usr ?></h1>
+              <svg fill="none" stroke="currentColor" id="copyCode" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"></path>
+              </svg>
+            </div>
+            <div id="qrcode"></div>
+          <?php else : ?>
+            <p><span id="url" class="url"><?php echo $url ?> </span><br></p>
+            <h1 class="usrCode"><?php echo $err ?></h1>
+          <?php endif; ?>
+
+          <script>
+            const url = "<?php echo $url ?>";
+            const code = "<?php echo $usr ?>";
+          </script>
+          <script src="<?php echo ROOT ?>/js/new.js"></script>
+
+        <?php endif; ?>
       </div>
     </div>
 </body>
