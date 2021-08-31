6f902eafa054bdba2b2276343f87593481f4319c
Filip Troníček <filip.tronicek@seznam.cz>
2021-08-31 20:04:39 +0000

    Admin redesign without mvp.css

--- public/admin.php
+++ public/admin.php
@@ -55,7 +55,6 @@ if ($isStaff) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard | Interclip</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/mvp.css@1.7.3/mvp.min.css">
     <link rel="stylesheet" href="<?php echo ROOT ?>/css/admin.css">
 </head>
 
@@ -76,61 +75,63 @@ if ($isStaff) {
             </section>
         <?php elseif ($isStaff) : ?>
             <section id="intro">
-                <main>
                     <h1>Hi, <?php echo $user["name"] ?? $user["nickname"] ?></h1>
                     <p>Welcome to the Interclip admin dashboard!<sup>BETA</sup></p>
-                </main>
-                <aside>
-                    <img alt="A lady sitting on a table" src="<?php echo ROOT ?>/img/graphics/fbdwm0.svg" height="150" />
-                    <h3>Interclip files</h3>
-                    <p>Total files: <strong id="files">-</strong></p>
-                    <p>Total size: <strong id="filesize">-</strong></p>
-                </aside>
-                <aside>
-                    <img alt="A server cluster" src="<?php echo ROOT ?>/img/graphics/1ahd5p.svg" height="150" />
-                    <h3>Server metrics</h3>
-                    <p>Server load: <strong><?php echo $systemLoad ?></strong></p>
-                    <p>Storage: <strong><?php echo (formatBytes(disk_total_space('/') - disk_free_space('/'))) . "/" . (formatBytes(disk_total_space('/'))) ?></strong></p>
-                    <p>Uptime: <strong><?php echo $uptime ?></strong></p>
-                </aside>
-                <aside>
-                    <img alt="A dashboard" src="<?php echo ROOT ?>/img/graphics/hggz12.svg" height="150" />
-                    <h3>Runtime details</h3>
-                    <p>Used memory: <strong><?php echo formatBytes(memory_get_usage()) ?></strong></p>
-                    <p>PHP version: <strong><?php echo phpversion(); ?></strong></p>
-                    <p>MySQL version: <strong><?php echo $mysqlVer ?></strong></p>
-                    <p>Running as: <strong><?php echo exec("whoami") ?></strong></p>
-                </aside>
-                <aside>
-                    <img alt="A person using a laptop" src="<?php echo ROOT ?>/img/graphics/szaqt9.svg" height="150" />
-                    <h3>Service stats</h3>
-                    <p>Clips: <strong><?php echo $count ?></strong></p>
-                    <p>Database rows: <strong><?php echo $totalLines ?></strong></p>
-                    <p>Redis items: <strong><?php echo getTotal() ?></strong></p>
-                </aside>
-                <aside>
-                    <img alt="A lady looking at a database symbol" src="<?php echo ROOT ?>/img/graphics/agmx1d.svg" height="150" />
-                    <h3>Service status</h3>
-                    <p>OS: <strong><?php echo PHP_OS ?></strong></p>
-                    <p>OS version: <strong><?php print_r($osinfo['version']) ?></strong></p>
-                    <p>Kernel: <strong><?php echo $kernel[0] ?></strong></p>
-                </aside>
+                    <section>
+                        <aside>
+                            <img alt="A lady sitting on a table" src="<?php echo ROOT ?>/img/graphics/fbdwm0.svg" height="150" />
+                            <h3>Interclip files</h3>
+                            <p>Total files: <strong id="files">-</strong></p>
+                            <p>Total size: <strong id="filesize">-</strong></p>
+                        </aside>
+                        <aside>
+                            <img alt="A server cluster" src="<?php echo ROOT ?>/img/graphics/1ahd5p.svg" height="150" />
+                            <h3>Server metrics</h3>
+                            <p>Server load: <strong><?php echo $systemLoad ?></strong></p>
+                            <p>Storage: <strong><?php echo (formatBytes(disk_total_space('/') - disk_free_space('/'))) . "/" . (formatBytes(disk_total_space('/'))) ?></strong></p>
+                            <p>Uptime: <strong><?php echo $uptime ?></strong></p>
+                        </aside>
+                        <aside>
+                            <img alt="A dashboard" src="<?php echo ROOT ?>/img/graphics/hggz12.svg" height="150" />
+                            <h3>Runtime details</h3>
+                            <p>Used memory: <strong><?php echo formatBytes(memory_get_usage()) ?></strong></p>
+                            <p>PHP version: <strong><?php echo phpversion(); ?></strong></p>
+                            <p>MySQL version: <strong><?php echo $mysqlVer ?></strong></p>
+                            <p>Running as: <strong><?php echo exec("whoami") ?></strong></p>
+                        </aside>
+                        <aside>
+                            <img alt="A person using a laptop" src="<?php echo ROOT ?>/img/graphics/szaqt9.svg" height="150" />
+                            <h3>Service stats</h3>
+                            <p>Clips: <strong><?php echo $count ?></strong></p>
+                            <p>Database rows: <strong><?php echo $totalLines ?></strong></p>
+                            <p>Redis items: <strong><?php echo getTotal() ?></strong></p>
+                        </aside>
+                        <aside>
+                            <img alt="A lady looking at a database symbol" src="<?php echo ROOT ?>/img/graphics/agmx1d.svg" height="150" />
+                            <h3>Service status</h3>
+                            <p>OS: <strong><?php echo PHP_OS ?></strong></p>
+                            <p>OS version: <strong><?php print_r($osinfo['version']) ?></strong></p>
+                            <p>Kernel: <strong><?php echo $kernel[0] ?></strong></p>
+                        </aside>
+                    </section>
             </section>
-        <?php elseif ($user !== false) : ?>
-            <?php http_response_code(403); ?>
-            <section id="intro">
-                <main>
-                    <h1>Yikes ¯\_(ツ)_/¯</h1>
-                    <p>Sorry, but you don't have the permissions to access this resource.</p>
-                </main>
-            </section>
-        <?php else : ?>
-            <?php
+    </main>
+
+<?php elseif ($user !== false) : ?>
+    <?php http_response_code(403); ?>
+    <section id="intro">
+        <main>
+            <h1>Yikes ¯\_(ツ)_/¯</h1>
+            <p>Sorry, but you don't have the permissions to access this resource.</p>
+        </main>
+    </section>
+<?php else : ?>
+    <?php
             http_response_code(401);
             header("Location: " . ROOT . "/login");
-            ?>
-        <?php endif; ?>
-    </main>
+    ?>
+<?php endif; ?>
+</main>
 </body>
 <script src="<?php echo ROOT; ?>/js/formatter.js" defer></script>
 <script src="<?php echo ROOT; ?>/js/admin.js"></script>
