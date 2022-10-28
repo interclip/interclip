42041342e29ef5d7e991a65ae0f0abdce2047d9d
Filip Troníček <filip@gitpod.io>
2022-10-28 19:23:03 +0000

    Remove dem analytics

--- includes/components/html/analytics.php
+++ includes/components/html/analytics.php
@@ -1,3 +1,3 @@
-<!-- Cloudflare Web Analytics -->
-<script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "0bf18f78dfbd40b2953f59d5d0034af2"}'></script>
+<!-- Analytics -->
+Cool that there are no analytics, huh?
 <!-- End Cloudflare Web Analytics -->
