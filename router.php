9445d5e51acd140e00847db6263aee2ab32f99f8
Filip Troníček <filip@gitpod.io>
2022-11-18 23:36:48 +0000

    Match code paths correctly

--- router.php
+++ router.php
@@ -9,71 +9,103 @@ use Pecee\SimpleRouter\SimpleRouter;
 
 SimpleRouter::group(['prefix' => ROOT], function () {
 
-    SimpleRouter::get('/', function () {
-        include_once "public/index.php";
-    });
+    SimpleRouter::all(
+        '/',
+        function () {
+            include_once "public/index.php";
+            return "";
+        }
+    );
 
     SimpleRouter::get('/receive/', function () {
         include_once "public/receive.php";
+        return "";
     });
 
     SimpleRouter::get('/file/', function () {
         include_once "public/file.php";
+        return "";
     });
 
     SimpleRouter::form('/upload/', function () {
         include_once "public/upload/index.php";
+        return "";
     });
 
     SimpleRouter::get('/admin/', function () {
         include_once "public/admin.php";
+        return "";
     });
 
     /* Clip manipulation */
 
     SimpleRouter::form('/get', function () {
         include_once "public/core/get.php";
+        return "";
     });
 
     SimpleRouter::form('/set', function () {
         include_once "public/core/set.php";
+        return "";
     });
 
     /* Static pages */
 
     SimpleRouter::get('/desktop/', function () {
         include_once "public/desktop.php";
+        return "";
     });
 
     SimpleRouter::get('/about/', function () {
         include_once "public/about.php";
+        return "";
     });
 
     /* Auth */
     SimpleRouter::get('/login/', function () {
         include_once "public/login.php";
+        return "";
     });
 
     SimpleRouter::get('/logout/', function () {
         include_once "public/logout.php";
+        return "";
     });
 
     SimpleRouter::get('/privacy/', function () {
         include_once "public/privacy.php";
+        return "";
     });
 
     /* API */
     SimpleRouter::match(['get', 'post'], '/api/set', function () {
         include_once "public/api/set.php";
+        return "";
     });
 
     SimpleRouter::match(['get', 'post'], '/api/get', function () {
         include_once "public/api/get.php";
+        return "";
     });
 
     SimpleRouter::match(['get', 'post'], '/api/file', function () {
         include_once "public/api/file.php";
+        return "";
     });
+
+    // Match clip codes
+    SimpleRouter::all(
+        '/code',
+        function ($user_code) {
+            include_once "components/get.php";
+            if (isset($url)) {
+                reDir($url);
+            } else {
+                $statusCode = 404;
+                include_once "includes/error.php";
+            }
+        }
+    )->setMatch('/^\/([a-z|0-9|A-Z]){5}\/?$/');
 
     /* Internal behavior */
 
