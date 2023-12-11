9f6db4ddeb5965ab9d320df6975be0949b434f5d
Filip Troníček <filip@gitpod.io>
2023-12-11 20:27:35 +0100

    Update copy

--- includes/components/html/settings.php
+++ includes/components/html/settings.php
@@ -42,7 +42,7 @@
       </label>
     </div>
     <h3>Danger zone</h3>
-    <p>Clear all the local data including settings and recently made clips.</p>
+    <p>Clear all local data, including settings and recently made clips. Note that this does not delete any actual clips on the server, just their local cache.</p>
     <button class="btn-remove" id="removeData">
       <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
